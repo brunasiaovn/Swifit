c067cf097653b850b971a16eb79a53ee122db305
Bruna <bruna.dev@hotmail.com>
2024-06-29 12:08:01 -0300

    Initial Commit

---
+++ dia1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dia1
+//
+//  Created by Bruna Novais on 29/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dia1/dia1App.swift
@@ -0,0 +1,17 @@
+//
+//  dia1App.swift
+//  dia1
+//
+//  Created by Bruna Novais on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct dia1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
